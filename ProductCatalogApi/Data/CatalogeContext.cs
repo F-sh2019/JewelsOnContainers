bd83e82ee1d5440e136df5c3953c8f14feb19323
Filicity SH <f.elena.sh@gmail.com>
2021-11-05 09:30:13 -0700

    Delete everything to test new project

---
+++ ProductCatalogApi/Data/CatalogeContext.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductCatalogApi.Domain;
+
+namespace ProductCatalogApi.Data
+{
+    public class CatalogeContext :DbContext  
+    {
+        public DbSet<CatalogType>  CatalogeTypes { get; set; }
+        public DbSet<CatalogBrand> CatalogBrands { get; set; }
+        public DbSet<CatalogItem> Catalog { get; set; }
+
+        public CatalogeContext(DbContextOptions options) : base(options)
+        { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<CatalogType>(e => {
+                    e.Property(t => t.Id)
+                    .IsRequired()
+                    .ValueGeneratedOnAdd();
+
+                    e.Property(t => t.Type)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                });
+
+
+
+                modelBuilder.Entity<CatalogBrand>(e =>
+               {
+                   e.Property(b => b.Brand)
+                     .IsRequired()
+                     .HasMaxLength(100);
+                   e.Property(b => b.Id)
+                     .IsRequired()
+                     .ValueGeneratedOnAdd();
+
+               });
+
+
+                modelBuilder.Entity<CatalogItem>(e =>
+                {
+                    e.Property(c => c.Id)
+                        .IsRequired()
+                        .ValueGeneratedOnAdd();
+
+                    e.Property(c => c.Name)
+                        .IsRequired()
+                        .HasMaxLength(100);
+
+                    e.Property(c => c.Price)
+                        .IsRequired();
+
+                    e.HasOne(C => C.CatalogType)
+                        .WithMany()
+                        .HasForeignKey(C => C.CatalogTypeId);
+                    e.HasOne(c => c.CatalogBrand)
+                        .WithMany()
+                        .HasForeignKey(c => c.CatalogBrandId);
+                });
+            
+            
+            
+        
+                
+                
+                
+             }
+    }
+}
---
+++ ProductCatalogApi/Migrations/20211025235845_Initial.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProductCatalogApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CatalogBrands",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Brand = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatalogBrands", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CatalogeTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatalogeTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Catalog",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PictureUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CatalogTypeId = table.Column<int>(type: "int", nullable: false),
+                    CatalogeBrandId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Catalog", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Catalog_CatalogBrands_CatalogeBrandId",
+                        column: x => x.CatalogeBrandId,
+                        principalTable: "CatalogBrands",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Catalog_CatalogeTypes_CatalogTypeId",
+                        column: x => x.CatalogTypeId,
+                        principalTable: "CatalogeTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Catalog_CatalogeBrandId",
+                table: "Catalog",
+                column: "CatalogeBrandId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Catalog_CatalogTypeId",
+                table: "Catalog",
+                column: "CatalogTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Catalog");
+
+            migrationBuilder.DropTable(
+                name: "CatalogBrands");
+
+            migrationBuilder.DropTable(
+                name: "CatalogeTypes");
+        }
+    }
+}
