2b3372ec55a233898686f46e0ae29b3ab5dfbb58
Filicity SH <f.elena.sh@gmail.com>
2021-11-06 17:59:16 -0700

    Did everything Create pic Api

---
+++ JewlesOnContainers/Data/CatalogContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProductCatalogApi.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductCatalogApi.Data
+{
+    public class CatalogContext:DbContext
+    {
+
+        public CatalogContext(DbContextOptions options) : base(options)
+        { }
+        public DbSet<CatalogType> CatalogTypes { get; set; }
+        public DbSet<CatalogBrand> CatalogBrands { get; set; }
+        public DbSet<CatalogItem> CatalogItems { get; set; }
+
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CatalogType>(e =>
+                {
+                    e.Property(t => t.Id).IsRequired()
+                                    .ValueGeneratedOnAdd();
+
+                    e.Property(t => t.Type).HasMaxLength(100);
+                });
+
+            modelBuilder.Entity<CatalogBrand>(e =>
+            {
+                e.Property(t => t.Id).IsRequired()
+                                     .ValueGeneratedOnAdd();
+
+                e.Property(t => t.Brand).HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<CatalogItem>(e =>
+            {
+
+                e.Property(c => c.Id).IsRequired()
+                                    .ValueGeneratedOnAdd();
+
+                e.Property(c => c.Name).IsRequired()
+                                    .HasMaxLength(100);
+
+
+                e.Property(c => c.Price).IsRequired();
+
+
+                e.HasOne(c => c.CatalogType).WithMany().HasForeignKey(c => c.CatalogTypeId);
+
+                e.HasOne(C => C.CatalogBrand).WithMany().HasForeignKey(c => c.CatalogBrandId);
+            });
+
+
+           
+        }
+    }
+}
---
+++ JewlesOnContainers/Program.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProductCatalogApi.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ProductCatalogApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            
+            using( var scope=host.Services.CreateScope())
+            {
+                var Serviceproviders = scope.ServiceProvider;
+                var context = Serviceproviders.GetRequiredService<CatalogContext>();
+                CatalogSeed.seed(context);
+             }
+
+                        
+
+                
+                host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
